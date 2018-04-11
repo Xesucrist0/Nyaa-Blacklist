44a119d050e355fe0f82690df05761cbd8fc03c6
Xesucrist0 <Xesucrist666@gmail.com>
2018-04-11 23:21:19 +0200

    Update NyaaBL.js

--- NyaaBL.js
+++ NyaaBL.js
@@ -7,30 +7,9 @@
 // @grant        none
 // ==/UserScript==
 
+// INSTRUCTIONS AT THE BOTTOM
 
-function hide_by_categories() {
-
-	var he = [];
-
-	//Uncomment the categories you want to hide
-
-	//he.push("Anime - Anime Music Video");
-	//he.push("Anime - Non-English-translated");
-	//he.push("Anime - English-translated");
-	//he.push("Anime - Raw");
-	//he.push("Audio - Lossy");
-	//he.push("Audio - Lossless");
-	//he.push("Literature - Raw");
-	//he.push("Literature - English-translated");
-	//he.push("Literature - Non-English-translated");
-	//he.push("Live Action - Raw");
-	//he.push("Live Action - English-translated");
-	//he.push("Live Action - Non-English-translated");
-	//he.push("Live Action - Idol/Promotional Video");
-	//he.push("Pictures - Graphics");
-	//he.push("Pictures - Photos");
-	//he.push("Software - Games");
-	//he.push("Software - Applications");
+function hide_by_categories(he) {
 
 	var x = document.getElementsByTagName("tr");
 	var i;
@@ -45,41 +24,88 @@ function hide_by_categories() {
 	}
 }
 
-function hide_by_keyword() {
-
-	// Add your blacklisted keywords in the array below
-	// Example: var blacklist_keywords = ["animeland", "leopard-raws", "vostfr", "puyasubs", "hevc", "1080p hi444"];
-
-	var blacklist_keywords = [];
+function hide_by_keyword(blacklist_keywords) {
 
 	var x = document.getElementsByTagName("tr");
 	var i;
 	var n;
+
 	for (i = 1; i < x.length; i++) {
-		var y = x[i].childNodes[3].childNodes[1].title;
+
+		if (x[i].childNodes[3].childNodes.length > 3) {
+			var y = x[i].childNodes[3].childNodes[3].title;
+		} else {
+			var y = x[i].childNodes[3].childNodes[1].title;
+		}
+
 		for (n = 0; n < blacklist_keywords.length; n++) {
+
 			if (y.toLowerCase().indexOf(blacklist_keywords[n].toLowerCase()) >= 0) {
+
 				x[i].style.display = "none";
 			}
 		}
 	}
 }
 
-//Add some document.on_ready() statement... maybe.
+function change_color(keywords, color) {
+	
+	var x = document.getElementsByTagName("tr");
+	var i;
+	var n;
+	var j;
 
-hide_by_categories(); 
-hide_by_keyword();
+	for (i = 1; i < x.length; i++) {
 
+		if (x[i].childNodes[3].childNodes.length > 3) {
+			var y = x[i].childNodes[3].childNodes[3].title;
+		} else {
+			var y = x[i].childNodes[3].childNodes[1].title;
+		}
 
+		for (n = 0; n < keywords.length; n++) {
 
+			if (y.toLowerCase().indexOf(keywords[n].toLowerCase()) >= 0) {
 
+				var z = x[i].getElementsByTagName("td");
+				for (j = 0; j < z.length; j++) {
 
+					z[j].style.backgroundColor = color;
+				}
+			}
+		}
+	}
+}
 
+var hidden_categories = [];
 
+// Uncomment the categories you want to hide.
 
+//hidden_categories.push("Anime - Anime Music Video");
+//hidden_categories.push("Anime - Non-English-translated");
+//hidden_categories.push("Anime - English-translated");
+//hidden_categories.push("Anime - Raw");
+//hidden_categories.push("Audio - Lossy");
+//hidden_categories.push("Audio - Lossless");
+//hidden_categories.push("Literature - Raw");
+//hidden_categories.push("Literature - English-translated");
+//hidden_categories.push("Literature - Non-English-translated");
+//hidden_categories.push("Live Action - Raw");
+//hidden_categories.push("Live Action - English-translated");
+//hidden_categories.push("Live Action - Non-English-translated");
+//hidden_categories.push("Live Action - Idol/Promotional Video");
+//hidden_categories.push("Pictures - Graphics");
+//hidden_categories.push("Pictures - Photos");
+//hidden_categories.push("Software - Games");
+//hidden_categories.push("Software - Applications");
 
+hide_by_categories(hidden_categories); 
 
+// Add an array of keywords. Those entries that contain one of these words will be hidden.
+// Example: hide_by_keyword(["anime-land", "hevc", "vostfr"]);
+hide_by_keyword([]);
 
-
-
-
+// add an array of keywords and the background color to replace with. Add as many as you want.
+// Example: change_color(["commie", "asenshi", "chyuu", "memesubs"], "#d9edf7");
+change_color([], "#ffcbe2"); //pink
+change_color([], "#d9edf7"); //blue
